010e5c9c0235a058334e58e33561edb4b6b7e406
hankaj <hanna.jarlaczynska@gmail.com>
2021-09-06 20:33:59 +0200

    Fix graph coloring

--- genetic_algorithms/problems/graph_coloring_problem/moves.py
+++ genetic_algorithms/problems/graph_coloring_problem/moves.py
@@ -1,5 +1,6 @@
 from genetic_algorithms.problems.base.moves import Move
 from genetic_algorithms.problems.graph_coloring_problem.state import GraphColoringState
+import copy
 
 
 class ChangeColor(Move[GraphColoringState]):
@@ -8,6 +9,6 @@ class ChangeColor(Move[GraphColoringState]):
         (self.idx, self.color) = idx, color
 
     def make(self) -> GraphColoringState:
-        new_coloring = [*self.state.coloring]
+        new_coloring = copy.deepcopy(self.state.coloring)
         new_coloring[self.idx].color = self.color
         return GraphColoringState(model=self.state.model, coloring=new_coloring)
