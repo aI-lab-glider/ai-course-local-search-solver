9516470fc6d434360d1b86554b4085efc223b92a
hankaj <hanna.jarlaczynska@gmail.com>
2021-09-08 09:24:42 +0200

    Add constraints to TSP move generator

--- genetic_algorithms/problems/traveling_salesman_problem/move_generator.py
+++ genetic_algorithms/problems/traveling_salesman_problem/move_generator.py
@@ -1,11 +1,9 @@
 import random
 from itertools import combinations
 
-
 from genetic_algorithms.models.move_generator import MoveGenerator
 from genetic_algorithms.problems.traveling_salesman_problem.state import TravelingSalesmanState
 from genetic_algorithms.problems.traveling_salesman_problem.moves import SwapEdges
-from genetic_algorithms.problems.traveling_salesman_problem.models.edge import Edge
 from typing import Generator
 
 
@@ -13,16 +11,24 @@ class TravelingSalesmanMoveGenerator(MoveGenerator):
     def __init__(self, depot_idx: int):
         self._depot_idx = depot_idx
 
-    def _is_depot_start(self, a: Edge):
-        return a.start != self._depot_idx
+    def _depot_start(self, move: SwapEdges):
+        return move.a.start != self._depot_idx
 
-    def _is_depot_end(self, b: Edge):
-        return b.end != self._depot_idx
+    def _depot_end(self, move: SwapEdges):
+        return move.b.end != self._depot_idx
+
+    def _satisfies_constraints(self, move: SwapEdges):
+        constraints = [self._depot_start, self._depot_end]
+        return all([constraint(move) for constraint in constraints])
 
     def random_move(self, state: TravelingSalesmanState) -> SwapEdges:
-        return SwapEdges(state, random.choice(list(state.edges)), random.choice(list(state.edges)))
+        generate_move = lambda: SwapEdges(state, random.choice(list(state.edges)), random.choice(list(state.edges)))
+        random_move = generate_move()
+        while not self._satisfies_constraints(random_move):
+            random_move = generate_move()
+        return random_move
 
     def available_moves(self, state: TravelingSalesmanState) -> Generator[SwapEdges, None, None]:
         return (SwapEdges(state, a, b) for a, b in combinations(state.edges, 2)
-                if not self._is_depot_start(a) and not self._is_depot_end(b))
+                if self._satisfies_constraints(SwapEdges(state, a, b)))
 
