9712977ba6a41f550770e65dbd2cf75c6cb80c8f
Bohdan <qwebeck5@gmail.com>
2021-09-15 08:15:06 +0200

    Fixed naming

--- genetic_algorithms/problems/traveling_salesman_problem/models/salesman.py
+++ genetic_algorithms/problems/traveling_salesman_problem/models/salesman.py
@@ -9,8 +9,8 @@ class Salesman(Point):
     travelled_distance: float = 0
 
     def walk_to(self, point: Point) -> "Salesman":
-        self.walked_distance += sqrt((point.x - self.x)
-                                     ** 2 + (self.y - point.y) ** 2)
+        self.travelled_distance += sqrt((point.x - self.x)
+                                        ** 2 + (self.y - point.y) ** 2)
         self.x, self.y = point.x, point.y
         return self
 
