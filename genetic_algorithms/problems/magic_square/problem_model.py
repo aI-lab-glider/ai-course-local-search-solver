3792f33dc933780c2bcd5ce485166458b8b119f5
oskar <oskartrybus@outlook.com>
2021-10-02 14:34:04 +0200

    Corrections

--- genetic_algorithms/problems/magic_square/problem_model.py
+++ genetic_algorithms/problems/magic_square/problem_model.py
@@ -5,11 +5,12 @@ from typing import Generator, List
 import random
 from genetic_algorithms.problems.magic_square.moves import SwapNumbers
 from genetic_algorithms.problems.magic_square.state import MagicSquareState
+import numpy as np
 
 
 class MagicSquareModel(Model):
-    def __init__(self, numbers: List[int], magic_number: int):
-        self._numbers: List[int] = numbers
+    def __init__(self, numbers: np.matrix, magic_number: int):
+        self._numbers: np.matrix = numbers
         self._magic_number = magic_number
         initial_solution = self._find_initial_solution()
         super().__init__(initial_solution)
@@ -19,46 +20,30 @@ class MagicSquareModel(Model):
         return self._numbers
 
     def _find_initial_solution(self) -> MagicSquareState:
-        random.shuffle(self._numbers)
+        np.random.shuffle(self._numbers)
         return MagicSquareState(model=self)
 
     def moves_for(self, state: MagicSquareState) -> Generator[SwapNumbers, None, None]:
-        indexes_to_change_list = []
-        for i in range(len(state.numbers)):
-            for j in range(len(state.numbers)):
-                if j>i:
-                    indexes_to_change_list.append((i, j))
-        return(SwapNumbers(state, a, b) for a, b in indexes_to_change_list)
-
+        return(SwapNumbers(state, [a, b], [c, d]) for a in range(len(state.numbers)) for b in range(len(state.numbers)) for c in range(len(state.numbers)) for d in range(len(state.numbers)) if d > b or (d == b and c > a))
 
     def cost_for(self, state: MagicSquareState) -> int:
-        size_of_square = math.sqrt(len(self._numbers))
-        size_of_square = int(size_of_square)
-        cost = 0
-        for i in range(size_of_square):
-            curr_row = [self._numbers[j] for j in range(i*size_of_square, (i*size_of_square)+1, 1)]
-            curr_col = [self._numbers[j] for j in range(i, len(self._numbers), size_of_square)]
-            current_row_sum = sum(curr_row)
-            current_column_sum = sum(curr_col)
-            cost += abs(current_column_sum-self._magic_number)
-            cost += abs(current_row_sum-self._magic_number)
-        left_diagonal_sum = 0
-        right_diagonal_sum = 0
-        for i in range(size_of_square):
-            left_diagonal_sum += self._numbers[i*(size_of_square+1)]
-            right_diagonal_sum += self._numbers[(size_of_square-1)+i*(size_of_square-1)]
-        cost += abs(left_diagonal_sum-self._magic_number)
-        cost += abs(right_diagonal_sum-self._magic_number)
+        row_cost_sum = abs(self._numbers.sum(axis=1) - self._magic_number).sum()
+        columnt_cost_sum = abs(self._numbers.sum(axis=0) - self._magic_number).sum()
+        left_diagonal_cost = np.trace(self._numbers) - self._magic_number
+        right_diagonal_cost = np.trace(np.fliplr(self._numbers)) - self._magic_number
+        cost = row_cost_sum + columnt_cost_sum + left_diagonal_cost + right_diagonal_cost
         return cost
 
     @staticmethod
     def from_benchmark(benchmark_name: str):
         with open(Path.cwd()/"genetic_algorithms"/"problems"/"magic_square"/"benchmarks"/benchmark_name) as benchmark_file:
             magic_number = int(benchmark_file.readline())
-            numbers = str(benchmark_file.readline())
-            numbers = numbers.split()
-            numbers = [int(x) for x in numbers]
+            numbers = benchmark_file.readlines()
+            list_of_np_matrixes = [np.matrix(numbers[x]) for x in range(len(numbers))]
+            output = list_of_np_matrixes[0]
+            for i in range(1, len(list_of_np_matrixes)):
+                output = np.vstack((output, list_of_np_matrixes[i]))
             return MagicSquareModel(
-                numbers=numbers,
+                numbers=output,
                 magic_number=magic_number
             )
