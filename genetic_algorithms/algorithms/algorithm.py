57c200bb64f9db0b53e15bbaf7b709a0574f4b52
Bohdan <qwebeck5@gmail.com>
2021-10-06 00:03:56 +0200

    Add notification on best state assign

--- genetic_algorithms/algorithms/algorithm.py
+++ genetic_algorithms/algorithms/algorithm.py
@@ -16,6 +16,7 @@ class OptimizationStrategy(Enum):
     Min = 'min'
     Max = 'max'
 
+
 @dataclass
 class AlgorithmConfig:
     max_steps_without_improvement: int = 10
@@ -36,9 +37,11 @@ class SubscribableAlgorithm(Algorithm):
         config = config or DEFAULT_CONFIG
         self.config = config
         self.steps_from_last_state_update = 0
-        self.best_cost, self.best_state = float('inf') if config.optimization_stategy == OptimizationStrategy.Min else float('-inf'), None
+        self.best_cost, self.best_state = float(
+            'inf') if config.optimization_stategy == OptimizationStrategy.Min else float('-inf'), None
         self._next_state_subscribers: List[AlgorithmNextStateSubscriber] = []
-        self._next_neighbour_subsribers: List[AlgorithmNextNeingbourSubscriber] = []
+        self._next_neighbour_subsribers: List[AlgorithmNextNeingbourSubscriber] = [
+        ]
 
     def __init_subclass__(cls) -> None:
         SubscribableAlgorithm.algorithms[camel_to_snake(cls.__name__)] = cls
@@ -50,6 +53,9 @@ class SubscribableAlgorithm(Algorithm):
         """
 
     def next_state(self, model: Model, state: State) -> Union[State, None]:
+        if self.best_state is None:
+            self.best_state = state
+            self._on_next_state(model, state)
         next_state = self._find_next_state(model, state)
         if next_state:
             self._update_algorithm_state(model, next_state)
@@ -61,18 +67,19 @@ class SubscribableAlgorithm(Algorithm):
             OptimizationStrategy.Min: op.lt,
             OptimizationStrategy.Max: op.gt,
         }[self.config.optimization_stategy](is_better_cost, is_better_than_cost)
-    
+
     def _is_cost_better_or_same(self, is_better_cost, is_better_than_cost) -> bool:
-        return self._is_cost_strictly_better(is_better_cost, is_better_than_cost) or is_better_cost == is_better_than_cost 
+        return self._is_cost_strictly_better(is_better_cost, is_better_than_cost) or is_better_cost == is_better_than_cost
 
     def _update_algorithm_state(self, model: Model, new_state: State):
+        if self.best_state is None:
+            self.best_state = new_state
         next_state_cost = model.cost_for(new_state)
         if self._is_cost_better_or_same(next_state_cost, self.best_cost) and self.best_state != new_state:
             self.best_cost, self.best_state = next_state_cost, new_state
             self.steps_from_last_state_update = 0
         else:
             self.steps_from_last_state_update += 1
-
 
     def _is_in_optimal_state(self):
         return self.steps_from_last_state_update >= self.config.max_steps_without_improvement
@@ -87,19 +94,16 @@ class SubscribableAlgorithm(Algorithm):
         for subsriber in self._next_neighbour_subsribers:
             subsriber.notify(model, from_state, next_neighbour)
 
-    
     def subsribe_to_state_update(self, subsriber: AlgorithmNextStateSubscriber):
         self._next_state_subscribers.append(subsriber)
-    
+
     def subscribe_to_neinghbour_enter(self, subsriber: AlgorithmNextNeingbourSubscriber):
         self._next_neighbour_subsribers.append(subsriber)
 
-    
     def _get_neighbours(self, model: Model, state: State, is_stohastic=False) -> Generator[State, None, None]:
         move_gen = model.move_generator.available_moves if not is_stohastic else model.move_generator.random_moves
         while True:
             for move in move_gen(state):
-                neighbour = move.make() 
+                neighbour = move.make()
                 self._on_next_neighbour(model, state, neighbour)
                 yield neighbour
-
